d756acd9a5f91962f640d4b21c8886179bd93dbb
reiser <leonzone58@gmail.com>
2019-01-14 18:48:18 +0800

    add BF

---
+++ src/strings/BruteForce.java
@@ -0,0 +1,42 @@
+package strings;
+
+/**
+ * Author: reiserx
+ * Date:2019/1/14
+ * Des:find substring use BruteForce
+ */
+public class BruteForce {
+    public int indexOf(String main, String pattern) {
+
+        char[] mainchars = main.toCharArray();
+        char[] pchars = pattern.toCharArray();
+
+        for (int i = 0; i < mainchars.length - pchars.length; i++) {
+            if (mainchars[i] == pchars[0]) {
+                if (pchars.length == 1) {
+                    return i;
+                } else {
+                    for (int j = 1; j < pchars.length; j++) {
+                        if (mainchars[i + j] != pchars[j]) {
+                            break;
+                        } else {
+                            if (j == pchars.length - 1) {
+                                return i;
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
+
+        return -1;
+    }
+
+
+    public static void main(String[] args) {
+        BruteForce bf = new BruteForce();
+        System.out.println(bf.indexOf("aabcdef", "abc"));
+//        "abcdef".indexOf()
+    }
+}
