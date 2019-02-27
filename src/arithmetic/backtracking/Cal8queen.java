2f4ba4cfac0faf1d2eb242197c04c3bc9f0a5c70
reiser <leonzone58@gmail.com>
2019-02-27 17:37:12 +0800

    add doc

--- src/arithmetic/backtracking/Cal8queen.java
+++ src/arithmetic/backtracking/Cal8queen.java
@@ -64,36 +64,11 @@ public class Cal8queen {
         System.out.println();
     }
 
-    public int maxW = Integer.MIN_VALUE; // 存储背包中物品总重量的最大值
-
-    // cw 表示当前已经装进去的物品的重量和；i 表示考察到哪个物品了；
-// w 背包重量；items 表示每个物品的重量；n 表示物品个数
-// 假设背包可承受重量 100，物品个数 10，物品重量存储在数组 a 中，那可以这样调用函数：
-// f(0, 0, a, 10, 100)
-    public void f(int i, int cw, int[] items, int n, int w) {
-        if (cw == w || i == n) { // cw==w 表示装满了 ;i==n 表示已经考察完所有的物品
-            System.out.println(maxW);
-            if (cw > maxW) {
-                maxW = cw;
-            }
-            return;
-        }
-        //1.放入书包
-        f(i + 1, cw, items, n, w);
-        //2.不放入书包
-        if (cw + items[i] <= w) {// 已经超过可以背包承受的重量的时候，就不要再装了
-            f(i + 1, cw + items[i], items, n, w);
-        }
-    }
 
 
     public static void main(String[] args) {
         Cal8queen queen = new Cal8queen();
-
-//        queen.cal8queen(0);
-        int[] a = {55, 2, 3, 4, 5, 6, 7, 8, 9, 1};
-        queen.f(0, 0, a, 10, 100);
-        System.out.println(queen.maxW);
+        queen.cal8queen(0);
 
     }
 
